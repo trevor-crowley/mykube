0633a81b467035cd31309d75a420f7f537c81685
trevor-crowley <trevor_crowley@hotmail.com>
2019-01-12 21:28:25 -0500

    get metalLB working

---
+++ ghost/nginx-ingress.sh
@@ -0,0 +1,1 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml
---
+++ ghost/nginx-manditory.sh
@@ -0,0 +1,1 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
