503dd45ad0bb948300c642052d9161066d088d26
trevor-crowley <trevor_crowley@hotmail.com>
2019-11-26 22:46:21 -0500

    update jhub

--- zArchive/jhub/jhub-upgrade.sh
+++ zArchive/jhub/jhub-upgrade.sh
@@ -1,9 +1,8 @@
 # Suggested values: advanced users of Kubernetes and Helm should feel
 # free to use different values.
 RELEASE=jhub
-NAMESPACE=jhub
 
-helm upgrade --install $RELEASE jupyterhub/jupyterhub \
-  --namespace $NAMESPACE  \
+helm upgrade $RELEASE jupyterhub/jupyterhub \
   --version=0.9-fe7b5df \
-  --values jhub-config.yaml
+  --values config.yaml
+
