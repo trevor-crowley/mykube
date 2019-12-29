03889bf08f05597cbc42537238894e3ffa782081
trevor <trevor@luigiserver.home>
2019-12-29 11:42:54 -0500

    backup rancher to airport

---
+++ rancher/admin/backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# backup VM, NFS, DBs
+
+#rpm -qa > packages
+
+if mountpoint -q /mnt/backup
+then
+	echo "Backup err:  already mounted!"
+else
+	echo "Backup Start: " $(date)
+
+	mount -t cifs -v //luigibase/Data /mnt/backup/ \
+		-o credentials=/home/trevor/.smbcredentials,rw,hard,nosetuids,noperm,vers=1.0,sec=ntlm
+
+	if mountpoint -q /mnt/backup
+	then
+		echo "Backing up Postgres DB..."
+		sudo sh -c 'pg_dump bitnami_redmine -U postgres > /nfs-root/backup/postgres/bitnami_redmine.sql'
+		echo "Backing up MySQL DB..."
+		sudo sh -c 'mysqldump \
+		--defaults-extra-file=/home/trevor/.mylogin.cnf \
+		--single-transaction --quick --lock-tables=false \
+		bitnami_mediawiki \
+		> /nfs-root/backup/mysql/bitnami_mediawiki.sql'
+
+		echo "Backing up NFS..."
+		time rsync -av --delete /nfs-root/ /mnt/backup/nfs-root/
+
+		umount /mnt/backup
+		echo "Backup done: " $(date)
+	else
+		echo "Backup err:  failed to mount drive"
+	fi
+fi
+
---
+++ rancher/admin/vmss.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# see https://blog.programster.org/kvm-external-snapshots
+DOMAIN="$1"
+TIMESTAMP=`date +%s`
+SNAPSHOT_NAME=$TIMESTAMP
+
+VM_FOLDER="/nfs-root/backup"
+SNAPSHOT_FOLDER="`echo $VM_FOLDER`/`echo $DOMAIN`/snapshots/`echo $TIMESTAMP`"
+mkdir -p $SNAPSHOT_FOLDER
+
+MEM_FILE="`echo $SNAPSHOT_FOLDER`/mem.qcow2"
+DISK_FILE="`echo $SNAPSHOT_FOLDER`/disk.qcow2"
+
+# Find out if running or not
+STATE=`virsh dominfo $DOMAIN | grep "State" | cut -d " " -f 11`
+
+if [ "$STATE" = "running" ]; then
+
+  virsh snapshot-create-as \
+    --domain $DOMAIN $SNAPSHOT_NAME \
+    --diskspec vda,file=$DISK_FILE,snapshot=external \
+    --memspec file=$MEM_FILE,snapshot=external \
+    --atomic
+
+else
+
+  virsh snapshot-create-as \
+    --domain $DOMAIN $SNAPSHOT_NAME \
+    --diskspec vda,file=$DISK_FILE,snapshot=external \
+    --disk-only \
+    --atomic
+
+fi
