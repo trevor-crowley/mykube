fa5dc1e0ae22243e970af0aea90bf167d53b1d16
trevor-crowley <trevor_crowley@hotmail.com>
2020-05-24 18:12:42 -0400

    jhub3 setup

--- rancher/jhub/jhub-deploy.sh
+++ rancher/jhub/jhub-deploy.sh
@@ -1,9 +1,9 @@
 # Suggested values: advanced users of Kubernetes and Helm should feel
 # free to use different values.
 RELEASE=jhub
-NAMESPACE=jhub
+NAMESPACE=jhub3
 
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
   --namespace $NAMESPACE  \
-  --version=0.8.2 \
+  --version=0.9.0 \
   --values jhub-config.yaml
