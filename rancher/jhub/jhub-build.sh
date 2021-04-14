b2c1b8e4dcdb14be2dc6220a98f28f2c234a2f05
trevor <trevor.crowley@henryschein.ca>
2021-04-14 19:20:47 -0400

    update newest version

--- rancher/jhub/jhub-build.sh
+++ rancher/jhub/jhub-build.sh
@@ -1,3 +1,3 @@
-docker build -t registry.tcrow.io/scipy-notebook:latest .
+docker build -t registry.tcrow.io/scipy-notebook:565af01 .
 echo 'run docker push registry.tcrow.io/scipy-notebook:latest'
 
